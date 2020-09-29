5542af9325f2d40a3e4d8495048aa208ae97e8f9
Rik Megens <rmegens@redhat.com>
2020-09-29 13:27:14 +0200

    updated README for demo2 updated README for demo3 added setup script for demo2 added setup script for demo3 updated tree listing for all demos fixed typo fixed typo added cleanup for demo2 added cleanup for demo3 added comment to user for cleanup script for demo3

--- demo3/remove_ocp_app.sh
+++ demo3/remove_ocp_app.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 # remove app from cluster
 
+# call out to user that projectname is fixed and tied to the setup script
+echo "Be sure you have created the app with the setup script since the projectname is hardcoded"
+
 # delete project where all resources are created. this cleans up all resources
 oc delete project webinar20200929
